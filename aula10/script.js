ea15c7c17f7c181c05817988ed0e471c52178d15
Marcos Davy Guedes Dias <m.davy.br@gmail.com>
2019-05-03 14:02:11 -0300

    Exercício aula 10

--- aula10/script.js
+++ aula10/script.js
@@ -1,4 +1,4 @@
- const getRandomInt = (min, max) => {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,4 +8,14 @@ const updateProfile = () => {
   // https://reqres.in/api/users/${id}
   // document.getElementById("nome").innerHTML = `${nome} ${sobrenome}`;
   // document.getElementById("avatar").src = avatar;
+
+  fetch('https://jsonplaceholder.typicode.com/photos/' + getRandomInt(1, 5000))
+    // .then(response => console.log(response))
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      document.getElementById("nome").innerHTML = `${json.title}`;
+      document.getElementById("avatar").src = `${json.thumbnailUrl}`;
+
+    })
 }
